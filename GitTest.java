f1c55a6c63193b12630739547c2c95aff316f8d9
flywiss <962669695.com>
2016-09-23 12:11:46 +0800

    hello,i will commit

---
+++ GitTest.java
@@ -0,0 +1,16 @@
+package com.lvmama.tnt.back.web;
+
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("go to the new  Word");
+		System.out.println("go to the new  Word");
+		System.out.println("frist");
+
+	}
+
+}
